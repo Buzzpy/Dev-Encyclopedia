39c2cccde98241211018bd8461fda5c18ec52281
hossien <94741775+hossien014@users.noreply.github.com>
2024-08-14 19:04:19 +0330

    add symmetric key card

--- script.js
+++ script.js
@@ -13,8 +13,9 @@
     <a href="a-link-to-an-article-on-the-topic-mentioned-for-beginners" id="modal-link" target="_blank"> ⌁—— Read more about it ——⌁</a>
   `
 */
- 
-const descriptions = {  codeSmell: `
+
+const descriptions = {
+  codeSmell: `
     <h2 id="modal-heading">Code Smell</h2>
     
     <p class="modal-paragraph">
@@ -840,7 +841,7 @@ const descriptions = {  codeSmell: `
     
     <a href="https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/about-pull-requests" id="modal-link" target="_blank"> ⌁—— Read more about it ——⌁</a>
   `,
-  
+
   agile: `
     <h2 id="modal-heading">Agile</h2>
     
@@ -2454,7 +2455,7 @@ const descriptions = {  codeSmell: `
     
     <a href="https://immutable-js.github.io/immutable-js/" id="modal-link" target="_blank"> ⌁—— Read more about it ——⌁</a>
   `,
-  
+
   dataDeduplication: `
     <h2 id="modal-heading">Data Deduplication</h2>
     
@@ -2790,59 +2791,82 @@ const descriptions = {  codeSmell: `
     
     <a href="https://gdpr.eu/" target="_blank"> ⌁—— Read more about it ——⌁</a>
 
-  `
+  `,
+  SymmetricKey:
+    `
+   <h2 id="modal-heading">Symmetric-key</h2>
+    
+    <p class="modal-paragraph">
+    In cryptography, a symmetric key is one that is used both to encrypt and decrypt information. This means that to decrypt information, one must have the same key that was used to encrypt it. The keys, in practice, represent a shared secret between two or more parties that can be used to maintain a private information link. This requirement that both parties have access to the secret key is one of the main drawbacks of symmetric key encryption, in comparison to public-key encryption.
+    </p>
+ 
+        <img class="responsive-image" src="https://upload.wikimedia.org/wikipedia/commons/6/61/Simple_symmetric_encryption.png" alt="Dictionary image">
+
+    <a href="https://en.wikipedia.org/wiki/Symmetric-key_algorithm" target="_blank"> ⌁—— Read more about it ——⌁</a>
+`
 };
 
-    function filterCards() {
-      const input = document.getElementById('searchInput');
-      const filter = input.value.toLowerCase();
-      const cards = document.getElementsByClassName('card');
+function filterCards()
+{
+  const input = document.getElementById('searchInput');
+  const filter = input.value.toLowerCase();
+  const cards = document.getElementsByClassName('card');
 
-      for (let i = 0; i < cards.length; i++) {
-        const title = cards[i].getElementsByClassName('card-title')[0];
-        if (title.innerText.toLowerCase().indexOf(filter) > -1) {
-          cards[i].style.display = '';
-        } else {
-          cards[i].style.display = 'none';
-        }
-      }
+  for (let i = 0; i < cards.length; i++)
+  {
+    const title = cards[i].getElementsByClassName('card-title')[0];
+    if (title.innerText.toLowerCase().indexOf(filter) > -1)
+    {
+      cards[i].style.display = '';
+    } else
+    {
+      cards[i].style.display = 'none';
     }
+  }
+}
 
-    function showModal(term) {
-      const modal = document.getElementById('modal');
-      const modalBody = document.getElementById('modal-body');
-      modalBody.innerHTML = descriptions[term];
-      modal.style.display = 'block';
-    }
-
-    function readMore() {
-      // Redirect to detailed explanation page (e.g., a blog or documentation)
-      window.open('https://example.com/detailed-explanation', '_blank');
-    }
-
-    function showAbout() {
-      const modal = document.getElementById('modal');
-      const modalBody = document.getElementById('modal-body');
-      modalBody.innerHTML = '<h2>About</h2><strong>IMPORTANT: Bookmarking or Pinning this webpage would help you get a quick overview of anything related to tech in seconds! </strong></p><p>This "Dev Encyclopedia" is a simple project that simplifies things.<p>This website has almost all the commonly used technical terms, concepts and even programming-language specific jargons explained and links provided for furthur reading.</p><p>Most importantly, it is ad-free, so basically, the best encyclopedia everr! *excited. too much.*</p><p>Builit with love, by Chenuli J.</p><img src="image/Chenuli Signature.png"></img>';
-      modal.style.display = 'block';
-    }
-    function showBuilders() {
-      const modal = document.getElementById('modal');
-      const modalBody = document.getElementById('modal-body');
-      modalBody.innerHTML = '<h3>A Note from the Builder</h3><p>If you find a flaw, love to sponsor or need help with learning something, my inbox is open : <a href="mailto:buzzpy123@gmail.com">buzzpy123@gmail.com<a></p><p>-><p><p>- I am a Python Developer especilizing in backend so my <strong> web design skills </strong> are <strong>terrible.</strong></p><p>- This project was built in a month, unlike Rome which took years to build. Which means this is on early stages of development, so why not to <strong>fork, star and contribute? </strong> </p>';
-      modal.style.display = 'block';
-    }
-    function showSponsors() {
-      const modal = document.getElementById('modal');
-      const modalBody = document.getElementById('modal-body');
-      modalBody.innerHTML = '<h3>Sponsors</h3><p> You can sponsor this project via my Ko-Fi profile : <a href="https://ko-fi.com/buzzpy">ko-fi.com/buzzpy<a></p> <p>Once you make any donatioon, I will get in touch with you and make sure you get a place in this "Sponsors" page! </p>';
-      modal.style.display = 'block';
-    }
-
-   // to close the modal
-function closeModal(event) {
+function showModal(term)
+{
   const modal = document.getElementById('modal');
-  if (event.target == modal) {
+  const modalBody = document.getElementById('modal-body');
+  modalBody.innerHTML = descriptions[term];
+  modal.style.display = 'block';
+}
+
+function readMore()
+{
+  // Redirect to detailed explanation page (e.g., a blog or documentation)
+  window.open('https://example.com/detailed-explanation', '_blank');
+}
+
+function showAbout()
+{
+  const modal = document.getElementById('modal');
+  const modalBody = document.getElementById('modal-body');
+  modalBody.innerHTML = '<h2>About</h2><strong>IMPORTANT: Bookmarking or Pinning this webpage would help you get a quick overview of anything related to tech in seconds! </strong></p><p>This "Dev Encyclopedia" is a simple project that simplifies things.<p>This website has almost all the commonly used technical terms, concepts and even programming-language specific jargons explained and links provided for furthur reading.</p><p>Most importantly, it is ad-free, so basically, the best encyclopedia everr! *excited. too much.*</p><p>Builit with love, by Chenuli J.</p><img src="image/Chenuli Signature.png"></img>';
+  modal.style.display = 'block';
+}
+function showBuilders()
+{
+  const modal = document.getElementById('modal');
+  const modalBody = document.getElementById('modal-body');
+  modalBody.innerHTML = '<h3>A Note from the Builder</h3><p>If you find a flaw, love to sponsor or need help with learning something, my inbox is open : <a href="mailto:buzzpy123@gmail.com">buzzpy123@gmail.com<a></p><p>-><p><p>- I am a Python Developer especilizing in backend so my <strong> web design skills </strong> are <strong>terrible.</strong></p><p>- This project was built in a month, unlike Rome which took years to build. Which means this is on early stages of development, so why not to <strong>fork, star and contribute? </strong> </p>';
+  modal.style.display = 'block';
+}
+function showSponsors()
+{
+  const modal = document.getElementById('modal');
+  const modalBody = document.getElementById('modal-body');
+  modalBody.innerHTML = '<h3>Sponsors</h3><p> You can sponsor this project via my Ko-Fi profile : <a href="https://ko-fi.com/buzzpy">ko-fi.com/buzzpy<a></p> <p>Once you make any donatioon, I will get in touch with you and make sure you get a place in this "Sponsors" page! </p>';
+  modal.style.display = 'block';
+}
+
+// to close the modal
+function closeModal(event)
+{
+  const modal = document.getElementById('modal');
+  if (event.target == modal)
+  {
     modal.style.display = 'none';
   }
 }
