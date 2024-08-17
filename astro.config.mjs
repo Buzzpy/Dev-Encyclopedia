87411b86d7ace087ed6c0e8813f631c21649d0ce
Chenuli Jayasinghe <buzzpy123@gmail.com>
2024-08-17 14:54:46 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://astro-devpedia.pages.dev/',
+});
