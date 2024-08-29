69e627a8f3109bb967bc345818297ad75c6c4f7c
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2024-08-29 14:21:31 +0530

    refactor: scripts related changes
    - scripts are now processed by astro as per [these](https://docs.astro.build/en/guides/client-side-scripts/#script-processing) rules
    - cards now use [Event Delegation](https://javascript.info/event-delegation) to avoid attaching a new event listener to each card

---
+++ src/scripts/cards.ts
@@ -0,0 +1,18 @@
+import { showModal } from "./modal";
+
+const cardContainer = document.querySelector(".card-container");
+
+cardContainer?.addEventListener("click", (e) => {
+  const target = e.target as HTMLElement | null;
+  const card = target?.closest(".card");
+  const isExplainBtn = card?.querySelector(".card > .explain-button") === target;
+
+  if (isExplainBtn) {
+    const title = card.getAttribute("data-title")!;
+    const description = card.getAttribute("data-description")!;
+    const imageUrl = card.getAttribute("data-image-url");
+    const resourceUrl = card.getAttribute("data-resource-url");
+
+    showModal(title, description, imageUrl, resourceUrl);
+  }
+});
