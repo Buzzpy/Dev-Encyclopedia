f798324c44f8eebd08427bb9c8051a3a5f08d399
Ray Mathew <ray10mathew@gmail.com>
2024-08-30 08:06:13 -0700

    added schema that JSON files must adhere to

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,18 @@ import { defineCollection, z } from "astro:content";
 
 const termsCollection = defineCollection({
   type: "data", // Use "content" for md/mdx files
-  // schema: // TODO: Define schema for the data. This will ensure our added terms adhere to a strict schema
+  schema: z.object({ // Define schema for the data. This will ensure our added terms adhere to a strict schema
+    title: z.string(),
+    subtext: z.string(),
+    categories: z.array(z.string()),
+    author: z.string(),
+    description: z.object({
+      title: z.string(),
+      texts: z.array(z.string()),
+      image: z.string(),
+      references: z.array(z.string())
+    })
+  })
 });
 
 export const collections = {
