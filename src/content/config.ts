c8d659e35ceeb7b157bd7a724642050c1cbe9571
Ray Mathew <ray10mathew@gmail.com>
2024-08-30 16:16:07 -0700

    all fields are mandatory, except image

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,14 +3,14 @@ import { defineCollection, z } from "astro:content";
 const termsCollection = defineCollection({
   type: "data", // Use "content" for md/mdx files
   schema: z.object({ // Define schema for the data. This will ensure our added terms adhere to a strict schema
-    title: z.string(),
-    subtext: z.string(),
+    title: z.string().min(1),
+    subtext: z.string().min(1),
     categories: z.array(z.string()).nonempty(),
-    author: z.string(),
+    author: z.string().min(1),
     description: z.object({
-      title: z.string(),
-      texts: z.array(z.string()),
-      image: z.string(),
+      title: z.string().min(1),
+      texts: z.array(z.string()).nonempty(),
+      image: z.string().optional(),
       references: z.array(z.string()).nonempty()
     })
   })
