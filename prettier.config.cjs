977437652e0c538aa224b927220ad66e513f1b3f
KC Ng <nkc900201@gmail.com>
2024-10-05 12:13:38 +0900

    feature: update comments and default settings for vscode

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  // WIP - standard configuration
+  // standard configuration
   ...require('prettier-config-standard'),
   pluginSearchDirs: [__dirname],
   plugins: [require.resolve('prettier-plugin-astro')],
