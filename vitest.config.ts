249d1be98d82bd128e017aa1eedcddd3e803f712
Chenuli Jayasinghe <buzzpy123@gmail.com>
2024-10-17 10:18:33 -0400

    Syncing Develop and Master - 17/10 (#171)
    * feature: install vitest as testing framework, setup
    > dev encyclopedia@0.0.1 test
    > vitest
    
     DEV  v2.1.2 /home/kc900201/open_source/dev-encyclopedia
    
     ❯ src/components/Hero.test.ts  (1 test | 1 failed) 80ms
       × Hero
         → Cannot read properties of undefined (reading 'forEach')
     ✓ src/layouts/BaseLayout.test.ts  (1 test) 66ms
    
     Test Files  1 failed | 1 passed (2)
          Tests  1 failed | 1 passed (2)
       Start at  18:58:25
       Duration  1.75s (transform 865ms, setup 0ms, collect 2.64s, tests 146ms, environment 1ms, prepare 191ms)
    
     FAIL  Tests failed. Watching for file changes...
           press h to show help, press q to quit
    
     RERUN  rerun all tests
    
     ✓ src/layouts/BaseLayout.test.ts  (1 test) 39ms
     ❯ src/components/Hero.test.ts  (1 test | 1 failed) 38ms
       × Hero
         → Cannot read properties of undefined (reading 'forEach')
    
     Test Files  1 failed | 1 passed (2)
          Tests  1 failed | 1 passed (2)
       Start at  19:00:32
       Duration  1.47s
    
     FAIL  Tests failed. Watching for file changes...
           press h to show help, press q to quit
    Cancelling test run. Press CTRL+c again to exit forcefully. script (packag.json), add in vitest.contig.ts config file
    
    * feature: update package-lock.json
    
    * feature: first attempt to create a test file for astro component
    
    * feature: create test file for Hero component
    
    * fix: amend run test command
    
    ---------
    
    Co-authored-by: KC Ng <nkc900201@gmail.com>

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+/// <reference types="vitest" />
+import { getViteConfig } from "astro/config"
+
+export default getViteConfig({
+  test: {
+    // Vitest configuration options
+    // use the APIs globally like Jest
+    globals: true,
+  }
+})
